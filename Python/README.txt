cf3caf31485fe44b07b794f12fb7aa04ed5fc055
bzeng1234 <55465349+bzeng1234@users.noreply.github.com>
2019-11-12 19:28:49 -0500

    Add files via upload

---
+++ Python/README.txt
@@ -0,0 +1,8 @@
+This contains the code for the Pi hub. 
+In order to connect to the HC-05 make sure the module is connected to the correct "COM" port to read from the bluetooth module.
+
+Import the following packages:
+-pyserial
+-pymysql
+-mysql-connector
+-datetime 
---
+++ Python/bluetooth.py
@@ -0,0 +1,41 @@
+import serial
+import time
+import datetime
+import mysql.connector
+import pymysql
+
+print("Start")
+
+bluetooth = serial.Serial('COM8', 9600, timeout = 1)
+
+cnx = pymysql.connect(host="clover.cclls6i3ttha.us-east-2.rds.amazonaws.com",port=3306,user="clover", password="clover1234", db="clover")
+cursor = cnx.cursor()
+
+add_tracker = ("INSERT INTO plant_monitor "
+               "(moisture_level, light_intensity, date_time) "
+               "VALUES (%s, %s, %s)")
+
+time.sleep(3)
+
+print("Connected")
+while(1):
+    now = datetime.datetime.now()
+    input_data = bluetooth.readline()
+
+    if(len(input_data) != 0):
+        data = input_data.decode()
+        print(data)
+        words = data.split()
+        print(words)
+        data_tracker = (words[0], words[1], str(now))
+        cursor.execute(add_tracker, data_tracker)
+        emp_no = cursor.lastrowid
+        cnx.commit()
+    else:
+        print("empty")
+
+bluetooth.close()
+cursot.close()
+cnx.close()
+
+print("done")
